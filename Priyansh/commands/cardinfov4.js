a1ea4c23c968156620e08b9d1b7bc1675464450b
danimalik1234786 <tayyabwahdrah88877@gmail.com>
2025-04-19 01:01:25 +0500

    Update cardinfov4.js

--- Priyansh/commands/cardinfov4.js
+++ Priyansh/commands/cardinfov4.js
@@ -1,104 +1,76 @@
-const fonts = "/cache/Play-Bold.ttf"
-const downfonts = "https://drive.google.com/u/0/uc?id=1uni8AiYk7prdrC7hgAmezaGTMH5R8gW8&export=download"
-const fontsLink = 45
-const fontsInfo = 28
-const colorName = "#000000"
 module.exports.config = {
-  name: "cardinfov4",
-  version: "2.0.0",
-  hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "Tạo card thông tin người dùng facebook",
-  commandCategory: "info",
-  usages: "",
-  cooldowns: 5,
-  dependencies: {
-    canvas: "",
-    axios: "",
-    "fs-extra": "",
-  },
+
+	name: "trump",
+
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "饾悘饾惈饾悽饾惒饾悮饾惂饾惉饾悺 饾悜饾悮饾悾饾惄饾惍饾惌",
+	description: "Comment on the board ( 汀掳 蜏蕱 汀掳)",
+	commandCategory: "edit-img",
+	usages: "trump [text]",
+	cooldowns: 10,
+	dependencies: {
+		"canvas":"",
+		 "axios":"",
+		 "fs-extra":""
+	}
 };
 
-module.exports.circle = async (image) => {
-  const jimp = global.nodemodule["jimp"];
-  image = await jimp.read(image);
-  image.circle();
-  return await image.getBufferAsync("image/png");
+module.exports.wrapText = (ctx, text, maxWidth) => {
+	return new Promise(resolve => {
+		if (ctx.measureText(text).width < maxWidth) return resolve([text]);
+		if (ctx.measureText('W').width > maxWidth) return resolve(null);
+		const words = text.split(' ');
+		const lines = [];
+		let line = '';
+		while (words.length > 0) {
+			let split = false;
+			while (ctx.measureText(words[0]).width >= maxWidth) {
+				const temp = words[0];
+				words[0] = temp.slice(0, -1);
+				if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
+				else {
+					split = true;
+					words.splice(1, 0, temp.slice(-1));
+				}
+			}
+			if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
+			else {
+				lines.push(line.trim());
+				line = '';
+			}
+			if (words.length === 0) lines.push(line.trim());
+		}
+		return resolve(lines);
+	});
+} 
+
+module.exports.run = async function({ api, event, args }) {
+	let { senderID, threadID, messageID } = event;
+	const { loadImage, createCanvas } = require("canvas");
+	const fs = global.nodemodule["fs-extra"];
+	const axios = global.nodemodule["axios"];
+	let pathImg = __dirname + '/cache/trump.png';
+	var text = args.join(" ");
+	if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
+	let getPorn = (await axios.get(`https://i.imgur.com/ZtWfHHx.png`, { responseType: 'arraybuffer' })).data;
+	fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
+	let baseImage = await loadImage(pathImg);
+	let canvas = createCanvas(baseImage.width, baseImage.height);
+	let ctx = canvas.getContext("2d");
+	ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+	ctx.font = "400 45px Arial";
+	ctx.fillStyle = "#000000";
+	ctx.textAlign = "start";
+	let fontSize = 250;
+	while (ctx.measureText(text).width > 2600) {
+		fontSize--;
+		ctx.font = `400 ${fontSize}px Arial, sans-serif`;
+	}
+	const lines = await this.wrapText(ctx, text, 1160);
+	ctx.fillText(lines.join('\n'), 60,165);//comment
+	ctx.beginPath();
+	const imageBuffer = canvas.toBuffer();
+	fs.writeFileSync(pathImg, imageBuffer);
+return api.sendMessage({ attachment: fs.createReadStream(pathImg) }, threadID, () => fs.unlinkSync(pathImg), messageID);        =
 }
-module.exports.run = async function ({ api, event, args, Users }) {
-  if ((this.config.credits) != "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭") { return api.sendMessage(`⚡️Displayed credits have changed`, event.threadID, event.messageID)}
-  let { senderID, threadID, messageID } = event;
-  const { loadImage, createCanvas } = require("canvas");
-  const request = require('request');
-  const fs = global.nodemodule["fs-extra"];
-  const axios = global.nodemodule["axios"];
-  const Canvas = global.nodemodule["canvas"];
-  let pathImg = __dirname + `/cache/${senderID}123${threadID}.png`;
-  let pathAvata = __dirname + `/cache/avtuserrd.png`;
-  /*                 */
-  if(event.type == "message_reply") { uid = event.messageReply.senderID }
-    else uid = event.senderID;
-    const res = await api.getUserInfoV2(uid); 
-  let getAvatarOne = (await axios.get(`https://graph.facebook.com/${uid}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-  let bg = (
-    await axios.get(encodeURI(`https://i.imgur.com/fBgFUr8.png`), {
-      responseType: "arraybuffer",
-    })
-  ).data;
-  fs.writeFileSync(pathAvata, Buffer.from(getAvatarOne, 'utf-8'));
-  avataruser = await this.circle(pathAvata);
-  fs.writeFileSync(pathImg, Buffer.from(bg, "utf-8"));
-
-/*-----------------download----------------------*/
-if(!fs.existsSync(__dirname+`${fonts}`)) { 
-      let getfont = (await axios.get(`${downfonts}`, { responseType: "arraybuffer" })).data;
-       fs.writeFileSync(__dirname+`${fonts}`, Buffer.from(getfont, "utf-8"));
-    };
-/*---------------------------------------------*/
-
-  let baseImage = await loadImage(pathImg);
-  let baseAvata = await loadImage(avataruser);
-  let canvas = createCanvas(baseImage.width, baseImage.height);
-  let ctx = canvas.getContext("2d");
-  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAvata, 100, 97, 255, 255);
-  if (!res.location || res.location === "Không Có Dữ Liệu") res.location = "Not Found";
-  if (!res.birthday || res.birthday === "Không Có Dữ Liệu") res.birthday = "Not Found";
-if (!res.relationship_status || res.relationship_status === "Không Có Dữ Liệu") res.relationship_status = "Not Found";
-  if (!res.follow || res.follow === "Không Có Dữ Liệu") res.follow = "Not Found";
-
-    var gender = res.gender == 'male' ? "male" : res.gender == 'female' ? "female" : "Not public";
-    var birthday = res.birthday ? `${res.birthday}` : "Not public";
-    var love = res.relationship_status ? `${res.relationship_status}` : "Not public"
-    var location = res.location ? `${res.location}` : "Not public"
-  Canvas.registerFont(__dirname+`${fonts}`, {
-        family: "Play-Bold"
-    });
-      ctx.font = `${fontsInfo}px Play-Bold`;
-      ctx.fillStyle = "#ffff";
-      ctx.textAlign = "start";
-         var fontSize = 20;
-      ctx.fillText(`» Name: ${res.name}`, 455, 172);
-      ctx.fillText(`» Sex: ${gender}`, 455, 208);
-      ctx.fillText(`» Follow: ${res.follow}`, 455, 244);
-      ctx.fillText(`» Relationship: ${love}`, 455, 281);
-      ctx.fillText(`» Birthday: ${birthday}`, 455, 320);
-      ctx.fillText(`» Location: ${location}`, 455, 357);
-      ctx.fillText(`» UID: ${uid}`, 455, 397);
-      ctx.font = `${fontsLink}px Play-Bold`;
-      ctx.fillStyle = "#ffff";
-      ctx.textAlign = "start";
-      fontSize = 20;
-      ctx.fillText(`» Profile: ${res.link}`, 19, 468);
-      ctx.beginPath();
-  const imageBuffer = canvas.toBuffer();
-  fs.writeFileSync(pathImg, imageBuffer);
-  fs.removeSync(pathAvata);
-  
-  return api.sendMessage(
-    { attachment: fs.createReadStream(pathImg) },
-    threadID,
-    () => fs.unlinkSync(pathImg),
-    messageID
-  );
-};
