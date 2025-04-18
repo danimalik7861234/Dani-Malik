fe9815e2e1c192b0d98d9bb3b3965460bd8a0570
danimalik1234786 <tayyabwahdrah88877@gmail.com>
2025-04-19 00:47:09 +0500

    Update baucuaca.js

--- Priyansh/commands/baucuaca.js
+++ Priyansh/commands/baucuaca.js
@@ -1,34 +1,74 @@
 module.exports.config = {
-	name: "baucuaca",
-	version: "0.0.1",
+	name: "janu",
+	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Games",
-	commandCategory: "games",
-	usages: "baucuaca 500",
+	credits: "Clarence-DK",
+	description: "",
+	commandCategory: "general",
+	usages: "text [text]",
 	cooldowns: 5,
+	dependencies: {
+		"canvas":"",
+		 "axios":"",
+		 "fs-extra":""
+	}
 };
 
-module.exports.run = async function({ api, event, args, Currencies }) {
-            let { threadID, messageID, senderID } = event;
-            const slotItems = ["Vote","Crab","Fish"];
-			let money = (await Currencies.getData(event.senderID)).money;
-			var coin = args.join(" ");
-			if (!coin) return api.sendMessage(`You have not entered the bet amount!`, threadID, messageID);
-			let win = false;
-			if (isNaN(coin)|| coin.indexOf("-") !== -1) return api.sendMessage(`Your bet amount is not a number, please review usage at ${prefix}help baucuaca`, threadID, messageID);
-			if (!coin) return api.sendMessage("The bet amount has not been entered!", threadID, messageID);
-			if (coin > money) return api.sendMessage(`Your amount is not enough`, threadID, messageID);
-			if (coin < 50) return api.sendMessage(`Your bet is too small, the minimum is 50$!`, threadID, messageID);
-			let number = [];
-			for (i = 0; i < 3; i++) number[i] = Math.floor(Math.random() * slotItems.length);
-			if (number[0] == number[1] && number[1] == number[2]) {
-				money *= 9;
-				win = true;
-			}
-				else if (number[0] == number[1] || number[0] == number[2] || number[1] == number[2]) {
-					money *= 2;
-					win = true;
+module.exports.wrapText = (ctx, text, maxWidth) => {
+	return new Promise(resolve => {
+		if (ctx.measureText(text).width < maxWidth) return resolve([text]);
+		if (ctx.measureText('W').width > maxWidth) return resolve(null);
+		const words = text.split(' ');
+		const lines = [];
+		let line = '';
+		while (words.length > 0) {
+			let split = false;
+			while (ctx.measureText(words[0]).width >= maxWidth) {
+				const temp = words[0];
+				words[0] = temp.slice(0, -1);
+				if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
+				else {
+					split = true;
+					words.splice(1, 0, temp.slice(-1));
 				}
-				(win) ? api.sendMessage(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\nYou have won\nReceive ${coin} dollar.`, threadID, () => Currencies.increaseMoney(senderID, parseInt(coin)), messageID) : api.sendMessage(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\nYou have loss\nThe amount you bet belongs to the house`, threadID, () => Currencies.decreaseMoney(senderID, parseInt(coin)), messageID);
-}
+			}
+			if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
+			else {
+				lines.push(line.trim());
+				line = '';
+			}
+			if (words.length === 0) lines.push(line.trim());
+		}
+		return resolve(lines);
+	});
+} 
+
+module.exports.run = async function({ api, event, args }) {
+	let { senderID, threadID, messageID } = event;
+	const { loadImage, createCanvas } = require("canvas");
+	const fs = global.nodemodule["fs-extra"];
+	const axios = global.nodemodule["axios"];
+	let pathImg = __dirname + '/cache/markngu.png';
+	var text = args.join(" ");
+	if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
+	let getPorn = (await axios.get(`https://imgur.com/k4YKpzx.jpg`, { responseType: 'arraybuffer' })).data;
+	fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
+	let baseImage = await loadImage(pathImg);
+	let canvas = createCanvas(baseImage.width, baseImage.height);
+	let ctx = canvas.getContext("2d");
+	ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+	ctx.font = "400 45px Arial";
+	ctx.fillStyle = "#000000";
+	ctx.textAlign = "start";
+	let fontSize = 45;
+	while (ctx.measureText(text).width > 2250) {
+		fontSize--;
+		ctx.font = `400 ${fontSize}px Arial, sans-serif`;
+	}
+	const lines = await this.wrapText(ctx, text, 440);
+	ctx.fillText(lines.join('\n'), 100, 350);//comment
+	ctx.beginPath();
+	const imageBuffer = canvas.toBuffer();
+	fs.writeFileSync(pathImg, imageBuffer);
+return api.sendMessage({ attachment: fs.createReadStream(pathImg) }, threadID, () => fs.unlinkSync(pathImg), messageID);        
+           }
