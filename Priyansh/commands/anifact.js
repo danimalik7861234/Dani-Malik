7eb49ed29399c57e009afb6d4368e9299abe8bd8
danimalik1234786 <tayyabwahdrah88877@gmail.com>
2025-04-19 00:31:43 +0500

    Update anifact.js

--- Priyansh/commands/anifact.js
+++ Priyansh/commands/anifact.js
@@ -1,33 +1,27 @@
 module.exports.config = {
-    name: "anifact",
-    version: "1.0.0",
-    hasPermision: 0,
-    credit: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "random animal fact by Juswa",
-    commandCategory: "random-img",
-    cooldowns: 0,
+  name: "cpldp",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "PREM BABU",
+  description: "Couple Dp photos",
+  commandCategory: "Random-IMG",
+  usages: "cpl dp",
+  cooldowns: 2,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+    
 };
 
-module.exports.run = async function({api, event, args, utils, Users, Threads}) {
-    try {
-        let axios = require('axios');
-        let fs = require("fs-extra");
-        let request = require("request");
-        var juswa = ['dog', 'cat', 'panda', 'fox', 'koala', 'bird', 'raccoon', 'kangaroo', 'red_panda'];
-        var juswa1 = juswa[Math.floor(Math.random() * juswa.length)];
-        let {threadID, senderID, messageID} = event;
-	const res = await axios.get(`https://some-random-api.ml/animal/${juswa1}`);
-	console.log(res.data);
-	var data = res.data;
-	let callback = function() {
-            return api.sendMessage({
-                body:`Facts about ${juswa1}: ${data.fact}`,
-                attachment: fs.createReadStream(__dirname + `/cache/image.png`)
-            }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/image.png`), event.messageID);
-        };
-		return request(encodeURI(data.image)).pipe(fs.createWriteStream(__dirname + `/cache/image.png`)).on("close", callback);
-		} catch (err) {
-        console.log(err)
-        return api.sendMessage(`Error`, event.threadID)
-    }
-}
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+    var link = [
+"https://i.imgur.com/g7woYNY.jpg","https://i.imgur.com/0jDiNmQ.jpg","https://i.imgur.com/3OX7sWP.jpg","https://i.imgur.com/IthNc1C.jpg","https://i.imgur.com/1RoN4la.jpg","https://i.imgur.com/vcfIO27.jpg","https://i.imgur.com/8yWRoMe.jpg","https://i.imgur.com/nku8dTF.jpg","https://i.imgur.com/V32qQb0.jpg","https://i.imgur.com/lkem5Gd.jpg","https://i.imgur.com/QIpV0AY.jpg","https://i.imgur.com/zdnDEtm.jpg","https://i.imgur.com/w7eKGSy.jpg","https://i.imgur.com/ONCJm5B.jpg","https://i.imgur.com/oQavLMr.jpg","https://i.imgur.com/MuBToNp.jpg","https://i.imgur.com/JrMY7j8.jpg","https://i.imgur.com/MauPoyi.jpg","https://i.imgur.com/t1B6vz1.jpg","https://i.imgur.com/VT200cX.jpg","https://i.imgur.com/9HTasfZ.jpg","https://i.imgur.com/waeDhYd.jpg","https://i.imgur.com/5dHsVO8.jpg","https://i.imgur.com/rrWIcrz.jpg","https://i.imgur.com/nEVUP1b.jpg","https://i.imgur.com/iHqdCMp.jpg","https://i.imgur.com/YHsbqM7.jpg","https://i.imgur.com/5ZQOCmT.jpg","https://i.imgur.com/AvoyQyk.jpg","https://i.imgur.com/MCuS0xn.jpg","https://i.imgur.com/c8yiwxR.jpg"
+     ];
+     var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝐃𝐀𝐍𝐈 𝐌𝐀𝐋𝐈𝐊💝`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
+   };
