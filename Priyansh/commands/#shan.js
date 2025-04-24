2fc9d8b849e28c7ddea369dfdd827c44b9c58c83
danimalik7861234 <danimalik12345786@gmail.com>
2025-04-24 21:02:15 +0500

    Update #shan.js

--- Priyansh/commands/#shan.js
+++ Priyansh/commands/#shan.js
@@ -13,13 +13,13 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var name = await Users.getNameUser(event.senderID);
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("shan khan") ||
-     react.includes("Shaan Khan") || react.includes("") || react.includes("") ||
-react.includes("") ||
-react.includes("") ||     
-react.includes("")) {
+  if(react.includes("admin") ||
+     react.includes("owner") || react.includes("") || react.includes("") ||
+react.includes("zabii") ||
+react.includes("Zabi") ||     
+react.includes("@Zabii Nawab")) {
     var msg = {
-        body: `${name} 𝑻𝑼𝑴 𝑵𝑬 𝑩𝑯𝑼𝑳𝑨𝒀𝑨 𝑶𝑼𝑹 𝑯𝑼𝑴 𝑪𝑯𝑨𝑳𝑬 𝑨𝑨𝒀𝑬💞✿`,attachment: fs.createReadStream(__dirname + `/noprefix//received_1236460.jpeg`)
+        body: `${name} 𝑻𝑼𝑴 𝑵𝑬 𝑩𝑯𝑼𝑳𝑨𝒀𝑨 𝑶𝑼𝑹 𝑯𝑼𝑴 𝑪𝑯𝑨𝑳𝑬 𝑨𝑨𝒀𝑬💞✿`,attachment: fs.createReadStream(__dirname + `/noprefix//1744920523849.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💋", event.messageID, (err) => {}, true)
